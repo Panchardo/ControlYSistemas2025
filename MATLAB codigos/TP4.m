1370d296e79a990a13c11127457f917f153b0ca6
Panchardo <panchodal867@gmail.com>
2025-04-06 00:40:53 -0300

    tp4 casi finalizado

--- MATLAB codigos/TP4.m
+++ MATLAB codigos/TP4.m
@@ -35,7 +35,7 @@ disp(['Número de muestras: ', num2str(length(t))]);
 
 %% EJ 2
 
-SNR = 0.5; % dB
+SNR = 10; % dB
 %Función awgn propia
 signal_ruidosa = my_awgn(signal, SNR);
 %Función awgn de Matlab
@@ -48,11 +48,36 @@ subplot(3,1,2); plot(t, signal_ruidosa); title('Señal con my\_awgn');
 subplot(3,1,3); plot(t, signal_awgn); title('Señal con awgn de MATLAB');
 
 
+
+%% EJ 4
+
+X = 2.5;
+sigma = 2.5/sqrt(2);
+
+for B = 1:32
+    B
+    SNR_ADC = 6.02*B - 20*log(X/sigma) + 10.8
+end
+
+%% EJ 5
+
+nivel_cuant = 10/2^12
+SNR_ej5 = 6.02 * 11 - 20*log(5 / 3.5 * sqrt(2)) + 10.8 % La señal no utiliza el rango entero del ADC, es decir, no es de excursión completa
+
+%% EJ 6
+
+% a ) Error por offset
+% b ) Error de linealización
+% c ) Error por factor de escala no lineal (ajustar el factor de escala,
+% como el que puse en Simulink a la salida del ADC dependiendo de donde caiga la lectura)
+% d ) Pérdida de códigos palabras
+%% Funciones
+
+
 function senal_ruidosa = my_awgn(signal, snr)
     var_senal = var(signal);
-    var_ruido = var_senal/10^(snr/10); %% SE ASUME QUE LA SEÑAL TIENE MEDIA 0, LA VARIANZA SOLO CAPTURA LA POTENCIA DE LA PARTE VARIABLE
+    var_ruido = var_senal/10^(snr/10) %% SE ASUME QUE LA SEÑAL TIENE MEDIA 0, LA VARIANZA SOLO CAPTURA LA POTENCIA DE LA PARTE VARIABLE
 
     ruido = sqrt(var_ruido) * randn(1,length(signal));
-
     senal_ruidosa = signal + ruido;
 end
